5c83625eee590f792bceae7599c35ad37332ac92
developerJR7 <137431610+developerJR7@users.noreply.github.com>
2024-07-23 18:14:02 -0300

    tela de registra-se

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,84 +1,67 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Separator } from '@/components/ui/separator'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Card, CardContent, CardTitle } from '@/components/ui/card'
+import homemalfasorteios from '@/assets/homem_alfa_sorteios.png'
 import IconLeafClover from '@/components/icons/leafClover'
-import IconCorrectSign from '@/components/icons/CorrectSign'
+import { LoaderCircle } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const Register: React.FC = () => {
-  const navigate = useNavigate()
-
-  const handleLogin = () => {
-    navigate('/login')
-  }
-  const CorrectPhase = [
-    { text: 'Criação de', title: 'Campanha' },
-    { text: 'Ranking diário da', title: 'Campanha' },
-    { text: 'Sistema para', title: 'Afiliados' },
-    { text: 'Histórico de', title: 'Vendas' },
-    { text: 'Criação de', title: 'Promoções' },
-    { text: 'Módulo de', title: 'Sorteio' }
-  ]
+  const [loading] = useState<boolean>(false)
 
   return (
     <div className="flex h-screen w-screen items-center justify-center overflow-x-hidden p-14">
-      <Card className="h-5/6 max-h-[514px] w-9/12">
-        <CardHeader>
-          <CardTitle>
-            <div className="flex items-center gap-2">
-              <div className="flex items-start gap-2">
-                <IconLeafClover className="size-8 fill-[#A0AEC0]" />
-                <h2 className="font-kanit m-0 flex  flex-col items-end p-0 text-3xl font-normal text-[#255E17] opacity-100">
-                  Alfa
-                  <span className="font-hubballi mt-[-8px] text-xs font-normal">
-                    Sorteios
-                  </span>
-                </h2>
-              </div>
-            </div>
-            <p className="font-Helvetica text-2xl font-bold text-[#255E17]">
-              Seja bem-vindo(a)
+      <Card className="h-5/6 max-h-[453px] w-9/12">
+        <CardTitle className="flex justify-center">
+          <IconLeafClover className="size-24 fill-[#255E17]" />
+        </CardTitle>
+        <CardContent className="flex justify-between">
+          <div className="flex w-6/12 flex-col items-start justify-start gap-2">
+            <p className="font-Helvetica text-xl font-bold text-[#255E17]">
+              Registre-se
             </p>
-          </CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col justify-between gap-8">
-          <p className="font-Helvetica w-6/12 text-base font-normal text-black">
-            Aqui estão algumas das funcionalidades que você encontra na plataforma.
-          </p>
-          <div className="grid grid-cols-3 grid-rows-2">
-            {CorrectPhase.map(({ text, title }) => (
-              <div className="flex items-center justify-start gap-2">
-                <IconCorrectSign color="#A0AEC0" size={24} />
-                <span className=" text-base font-normal text-black">{text}</span>
-                <span className=" text-base font-semibold  text-black">{title}</span>
-              </div>
-            ))}
+            <p className="font-Helvetica text-xs font-normal text-black">
+              Preencha os campos para registrar-se
+            </p>
+            <div className="flex h-full w-full items-end justify-center">
+              <img
+                src={homemalfasorteios}
+                alt="Foto do homem Alfa Sorteios"
+                className="w-56 object-cover"
+              />
+            </div>
           </div>
-          <p className="text-base font-normal  text-black">
-            Desfrute de uma experiência sem complicações com nossa plataforma, 100%
-            intuitiva e fácil de usar. E quando precisar, conte com a nossa dedicada
-            equipe de suporte, sempre pronta para ajudar você em cada passo do
-            caminho.
-          </p>
-          <Separator className=" w-[52%] border-l border-[#A0AEC0]" />
+          <form className="flex w-6/12 flex-col gap-2 text-xs">
+            <Input placeholder="Nome completo" />
+            <Input placeholder="E-mail" />
+            <Input placeholder="Senha" />
+            <Input placeholder=" Repita sua Senha" />
+            <Input placeholder="(00) 0 0000-0000" />
+            <div className="flex gap-2">
+              <input type="radio" />
+              <label className="text-[10px]">
+                Ao se registrar, você aceita os{' '}
+                <span className="font-semibold">Termos de Uso</span> e nossa{' '}
+                <span className="font-semibold">Política de Privacidade</span>
+              </label>
+            </div>
+            <div className="flex items-center justify-between">
+              <Link to={'/login'}> {`< Fazer Login `}</Link>
+              <Button
+                className="rounded-lg bg-gradient-to-r from-[#FEEA8C] to-[#F9D94B] px-4 py-2  font-semibold text-[#3D3D3D] transition-all duration-300 hover:text-white"
+                //disabled={!isFormValid}
+                //onClick={handleLogin}
+              >
+                {loading ? (
+                  <LoaderCircle className="h-10 w-10 animate-spin text-white transition-transform" />
+                ) : (
+                  'Registrar-se'
+                )}
+              </Button>
+            </div>
+          </form>
         </CardContent>
-        <CardFooter className="flex flex-col items-start">
-          <p className="font-Helvetica text-2xl font-bold  text-[#255E17]">
-            Comece agora!
-          </p>
-          <p className=" text-base font-normal  text-black">
-            Faça seu{' '}
-            <button className="font-bold text-black underline" onClick={handleLogin}>
-              Login
-            </button>
-          </p>
-        </CardFooter>
       </Card>
     </div>
   )

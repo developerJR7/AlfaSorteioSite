4adc899b9beac7df5793b7aadf8f0604bd7d02e3
Alfafis <ctpmmg2009_101@hotmail.com>
2024-10-09 01:08:15 -0300

    box promotions

--- src/pages/Promotions/ConfigPromotions.tsx
+++ src/pages/Promotions/ConfigPromotions.tsx
@@ -32,123 +32,125 @@ const ConfigPromotions: React.FC = () => {
 
   return (
     <Container>
-      <h4 className="mb-4 flex items-center text-xl font-semibold text-[#255E17]">
-        <IconDiscount className="mr-2 size-7" />
-        Promoções
-      </h4>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-4 text-sm font-normal text-black"
-      >
-        <div className="flex flex-col gap-2">
-          <label htmlFor="selectOption" className="font-bold">
-            Selecione a Campanha que quer incluir a promoção
-          </label>
-          <Select onValueChange={setSelectOption} value={selectOption}>
-            <SelectTrigger className="bg-white shadow-sm">
-              <SelectValue placeholder="Selecione uma opção" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="option1">Opção 1</SelectItem>
-              <SelectItem value="option2">Opção 2</SelectItem>
-              <SelectItem value="option3">Opção 3</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label htmlFor="input1" className="font-bold">
-            Nome da Promoção*
-          </label>
-          <Input
-            id="input1"
-            type="text"
-            placeholder="Input 1"
-            value={input1}
-            onChange={(e) => setInput1(e.target.value)}
-            className="bg-white shadow-sm"
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label htmlFor="input2" className="font-bold">
-            Descrição da Promoção*
-          </label>
-          <Input
-            id="input2"
-            type="text"
-            placeholder="Input 2"
-            value={input2}
-            onChange={(e) => setInput2(e.target.value)}
-            className="bg-white shadow-sm"
-          />
-        </div>
-
-        <div className="grid grid-cols-2 gap-8">
-          <div className="flex flex-col gap-2">
-            <label htmlFor="input3" className="font-bold">
-              Data de Início da Promoção
-            </label>
-            <Input
-              id="input3"
-              type="text"
-              placeholder="Input 3"
-              value={input3}
-              onChange={(e) => setInput3(e.target.value)}
-              className="bg-white shadow-sm"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label htmlFor="input4" className="font-bold">
-              Data do final da Promoção
-            </label>
-            <Input
-              id="input4"
-              type="text"
-              placeholder="Input 4"
-              value={input4}
-              onChange={(e) => setInput4(e.target.value)}
-              className="bg-white shadow-sm"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label htmlFor="input5" className="font-bold">
-              Quantidade Mínima de Números para Desconto
-            </label>
-            <Input
-              id="input5"
-              type="text"
-              placeholder="Input 5"
-              value={input5}
-              onChange={(e) => setInput5(e.target.value)}
-              className="bg-white shadow-sm"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label htmlFor="input6" className="font-bold">
-              Preço Promocional por Número
-            </label>
-            <Input
-              id="input6"
-              type="text"
-              placeholder="Input 6"
-              value={input6}
-              onChange={(e) => setInput6(e.target.value)}
-              className="bg-white shadow-sm"
-            />
-          </div>
-        </div>
-
-        <Button
-          type="submit"
-          className="h-10 w-32 rounded-md bg-gradient-to-r from-[#FEEA8C] to-[#F9D94B] px-4 py-2 font-semibold text-slate-950 transition-all duration-300 hover:text-white"
+      <div className="flex h-dvh flex-col gap-2 rounded-xl border-2 border-[#A0AEC0] p-6 text-base">
+        <h4 className="mb-4 flex items-center text-xl font-semibold text-[#255E17]">
+          <IconDiscount className="mr-2 size-7" />
+          Promoções
+        </h4>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 text-sm font-normal text-black"
         >
-          Avançar
-        </Button>
-      </form>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="selectOption" className="font-bold">
+              Selecione a Campanha que quer incluir a promoção
+            </label>
+            <Select onValueChange={setSelectOption} value={selectOption}>
+              <SelectTrigger className="bg-white shadow-sm">
+                <SelectValue placeholder="Selecione uma opção" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="option1">Opção 1</SelectItem>
+                <SelectItem value="option2">Opção 2</SelectItem>
+                <SelectItem value="option3">Opção 3</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="input1" className="font-bold">
+              Nome da Promoção*
+            </label>
+            <Input
+              id="input1"
+              type="text"
+              placeholder="Input 1"
+              value={input1}
+              onChange={(e) => setInput1(e.target.value)}
+              className="bg-white shadow-sm"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="input2" className="font-bold">
+              Descrição da Promoção*
+            </label>
+            <Input
+              id="input2"
+              type="text"
+              placeholder="Input 2"
+              value={input2}
+              onChange={(e) => setInput2(e.target.value)}
+              className="bg-white shadow-sm"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 gap-8">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="input3" className="font-bold">
+                Data de Início da Promoção
+              </label>
+              <Input
+                id="input3"
+                type="text"
+                placeholder="Input 3"
+                value={input3}
+                onChange={(e) => setInput3(e.target.value)}
+                className="bg-white shadow-sm"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="input4" className="font-bold">
+                Data do final da Promoção
+              </label>
+              <Input
+                id="input4"
+                type="text"
+                placeholder="Input 4"
+                value={input4}
+                onChange={(e) => setInput4(e.target.value)}
+                className="bg-white shadow-sm"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="input5" className="font-bold">
+                Quantidade Mínima de Números para Desconto
+              </label>
+              <Input
+                id="input5"
+                type="text"
+                placeholder="Input 5"
+                value={input5}
+                onChange={(e) => setInput5(e.target.value)}
+                className="bg-white shadow-sm"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="input6" className="font-bold">
+                Preço Promocional por Número
+              </label>
+              <Input
+                id="input6"
+                type="text"
+                placeholder="Input 6"
+                value={input6}
+                onChange={(e) => setInput6(e.target.value)}
+                className="bg-white shadow-sm"
+              />
+            </div>
+          </div>
+
+          <Button
+            type="submit"
+            className="h-10 w-32 rounded-md bg-gradient-to-r from-[#FEEA8C] to-[#F9D94B] px-4 py-2 font-semibold text-slate-950 transition-all duration-300 hover:text-white"
+          >
+            Avançar
+          </Button>
+        </form>
+      </div>
     </Container>
   )
 }

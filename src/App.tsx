9912d516698d15b97ae757f92be04647e2690bec
developerJR7 <137431610+developerJR7@users.noreply.github.com>
2024-11-14 13:27:24 -0300

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useAlfa } from './contexts/UserContent'
-import { RotasPrivadas } from './routes/RotasPrivadas'
+
 import { RotasPublicas } from './routes/RotasPublicas'
 
 const App: React.FC = () => {
@@ -10,12 +10,7 @@ const App: React.FC = () => {
     checkUserIsAuthenticated()
   }, [])
 
-  return (
-    <>
-      {isAuthenticated === true && <RotasPrivadas />}
-      {isAuthenticated === false && <RotasPublicas />}
-    </>
-  )
+  return <>{isAuthenticated === false && <RotasPublicas />}</>
 }
 
 export default App
